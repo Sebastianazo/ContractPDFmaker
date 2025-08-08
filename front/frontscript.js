8a93b1e85b0aad2c6bd45f8a862caf7f58fce5b6
Sebastianazo <andresleiva7u7@gmail.com>
2025-08-08 19:19:34 -0300

    Update frontscript.js
    Mejora logica y entrega

--- front/frontscript.js
+++ front/frontscript.js
@@ -1,19 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Variables para el autocompletado de la fecha
     const fecha = document.getElementById("fecha");
     const date = new Date();
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0'); 
-    // Variables para el CheckMark
-
+    const day = String(date.getDate()).padStart(2, '0');
     const formattedDate = `${year}-${month}-${day}`;
+    fecha.value = formattedDate;
 
-    fecha.value = formattedDate; 
+    // Referencia al formulario
+    const form = document.getElementById('pdfForm');
 
     async function handleSubmit(event) {
         event.preventDefault();
-        const formData = new FormData(document.getElementById('pdfForm'));
+
+        // Limpiar puntos en inputs específicos
+        ['PCuotas', 'Cuotas', 'Cantidad'].forEach(id => {
+            const input = form.querySelector(`[name="${id}"]`);
+            if (input) {
+                input.value = input.value.replace(/\./g, ''); 
+            }
+        });
+
+        const formData = new FormData(form);
 
         try {
             const response = await fetch('/generate-pdf', {
@@ -25,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const blob = await response.blob();
                 const link = document.createElement('a');
                 link.href = URL.createObjectURL(blob);
-                link.download = 'Contrato.pdf';
+                link.download = 'Contrato_KingHogar.pdf';
                 link.click();
             } else {
                 alert('Error al generar el PDF');
@@ -35,4 +43,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    // Asociar el evento submit al formulario
+    form.addEventListener('submit', handleSubmit);
 });
+
+
