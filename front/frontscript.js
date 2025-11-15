97a14d81c24629b1f8c0ca455785f8f293e6255b
Sebastianazo <andresleiva7u7@gmail.com>
2025-11-15 01:32:57 -0300

    Implement product addition and removal in form
    Added functionality to dynamically add products to the form and handle their removal.

--- front/frontscript.js
+++ front/frontscript.js
@@ -7,14 +7,87 @@ document.addEventListener("DOMContentLoaded", function () {
     const formattedDate = `${year}-${month}-${day}`;
     fecha.value = formattedDate;
 
-    // Referencia al formulario
+    const AddProductBtn = document.getElementById("addProductBtn");
     const form = document.getElementById('pdfForm');
+
+
+    AddProductBtn.addEventListener("click", () => {
+     const Producto = document.getElementById("Producto");
+     const Cantidad = document.getElementById("Cantidad");
+     const PCuotas = document.getElementById("PCuotas");
+     const Cuotas = document.getElementById("Cuotas");
+
+     if (!Producto.value || !Cantidad.value || !PCuotas.value || !Cuotas.value) {
+        alert("Complete los campos, si el producto no es mayor de 1 cuota, coloque 1 en el campo Cuota.");
+        return;
+     };
+
+    const hiddenProducto = document.createElement("input");
+    hiddenProducto.type = "hidden";
+    hiddenProducto.name = "Producto[]";
+    hiddenProducto.value = Producto.value;
+
+    const hiddenCantidad = document.createElement("input");
+    hiddenCantidad.type = "hidden";
+    hiddenCantidad.name = "Cantidad[]";
+    hiddenCantidad.value = Cantidad.value;
+
+    const hiddenPCuotas = document.createElement("input");
+    hiddenPCuotas.type = "hidden";
+    hiddenPCuotas.name = "PCuotas[]";
+    hiddenPCuotas.value = PCuotas.value;
+
+    const hiddenCuotas = document.createElement("input");
+    hiddenCuotas.type = "hidden";
+    hiddenCuotas.name = "Cuotas[]";
+    hiddenCuotas.value = Cuotas.value;
+
+    form.appendChild(hiddenProducto);
+    form.appendChild(hiddenCantidad);
+    form.appendChild(hiddenPCuotas);
+    form.appendChild(hiddenCuotas);
+    
+    const sectorProducto = document.createElement("div");
+    const logTexto = document.createElement("p");
+    const botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "Eliminar";
+
+    logTexto.textContent = `Producto: ${Producto.value} | Cantidad: ${Cantidad.value} | Precio por Cuota: ${PCuotas.value} | Cuotas: ${Cuotas.value}`;
+    sectorProducto.appendChild(logTexto);
+    sectorProducto.appendChild(botonEliminar);
+    
+    logTexto.classList.add("textoProducto");
+    botonEliminar.classList.add("btnEliminarProducto");
+    sectorProducto.classList.add("sectorProductoTemp");
+    
+    
+    // Aca agrego el boton dentro de otro addEventListener para que tome el valor actualizado
+    botonEliminar.addEventListener("click", () => {
+        sectorProducto.remove();
+        form.removeChild(hiddenProducto);
+        form.removeChild(hiddenCantidad);
+        form.removeChild(hiddenPCuotas);
+        form.removeChild(hiddenCuotas);
+    });
+
+
+    document.getElementById("Sproducto").appendChild(sectorProducto)
+
+
+     Producto.value = "";
+     Cantidad.value = "";
+     PCuotas.value = "";
+     Cuotas.value = "";
+
+    Producto.focus();
+    });
+
 
     async function handleSubmit(event) {
         event.preventDefault();
 
         // Limpiar puntos en inputs específicos
-        ['PCuotas', 'Cuotas', 'Cantidad'].forEach(id => {
+        ['PCuotas[]', 'Cuotas[]', 'Cantidad[]'].forEach(id => {
             const input = form.querySelector(`[name="${id}"]`);
             if (input) {
                 input.value = input.value.replace(/\./g, ''); 
@@ -46,5 +119,3 @@ document.addEventListener("DOMContentLoaded", function () {
     // Asociar el evento submit al formulario
     form.addEventListener('submit', handleSubmit);
 });
-
-
